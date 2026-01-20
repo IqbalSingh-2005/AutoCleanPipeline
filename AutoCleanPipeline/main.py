1ee88f2bb2cabb7d6d92f482d6acc1fa918d40f3
Blackstate9080 <is7636665@gmail.com>
2026-01-21 00:30:16 +0530

    Day 2

--- AutoCleanPipeline/main.py
+++
@@ -1,9 +0,0 @@
-from src.loader import FileLoader
-
-raw_data = r"D:\project\AutoCleanPipeline\AutoCleanPipeline\test\Salary.csv"
-
-
-loader = FileLoader()
-
-df = loader.load(raw_data)
-print(df.head())
