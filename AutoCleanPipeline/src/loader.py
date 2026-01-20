3b341bfbfa5c16a9712128116aa05651f9fc56c5
Blackstate9080 <is7636665@gmail.com>
2026-01-20 13:57:50 +0530

    Reading Method Updated

--- AutoCleanPipeline/src/loader.py
+++ AutoCleanPipeline/src/loader.py
@@ -1,23 +1,105 @@
 import pandas as pd
 
-
-
 class FileLoader:
-   
-    def read_csv(self, path):
-        return pd.read_csv(path)
 
-    def read_excel(self, path):
-        return pd.read_excel(path)
-
-    def read_json(self, path):
-        return pd.read_json(path)
-                
-
-    def load_data(file_path: str) -> pd.DataFrame:
+    def read_csv(self, file_path: str) -> pd.DataFrame:
+        
         try:
-            df  = pd.read_csv(file_path)
+            df = pd.read_csv(file_path)
             print(f"Loaded data with Shape: {df.shape}")
             return df
-         except Exception as e:
-            raise RuntimeError(f"Failed to Load CSV: {e}")
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_excel(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_excel(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_json(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_json(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_fwf(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_fwf(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_html(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_html(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_hdf(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_hdf(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_feather(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_hdf(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_parquet(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_parquet(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_orc(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_orc(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_pickle(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_pickle(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+
+    def read_parquet(self, file_path: str) -> pd.DataFrame:
+        
+        try:
+            df = pd.read_sql(file_path)
+            print(f"Loaded data with Shape: {df.shape}")
+            return df
+        except Exception as e:
+             raise RuntimeError(f"Failed to Load CSV: {e}")
+                
+
+    
