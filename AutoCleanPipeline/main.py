5b17e19018e9bd0edd61760f15c57533488ba06d
Blackstate9080 <is7636665@gmail.com>
2026-01-23 00:43:37 +0530

    Updated Data Type Corrector Day 4

--- AutoCleanPipeline/main.py
+++ AutoCleanPipeline/main.py
@@ -9,6 +9,7 @@ c_clean = column_cleaner()
 raw_data = loader.upload_file()
 df = loader.load(str(raw_data))
 df = c_clean.stdn(df)
+df = c_clean.data_type_convt(df)
 df = c_clean.missing_data(df)
 
 print(df.head())
--- AutoCleanPipeline/src/cleaner.py
+++ AutoCleanPipeline/src/cleaner.py
@@ -1,13 +1,9 @@
-from pandas._libs.lib import is_integer
-
-from src.loader import FileLoader
-
 import pandas as pd
 import numpy as np
-import re
+from pandas.api.types import is_string_dtype
 
 class column_cleaner:
-
+ 
     def stdn(self, df: pd.DataFrame) -> pd.DataFrame:
         
         df = df.copy()
@@ -24,15 +20,40 @@ class column_cleaner:
     def missing_data(self, df: pd.DataFrame) -> pd.DataFrame:
       
         df = df.copy()
-
-    
         for col in df : 
+            if (
+                pd.api.types.is_integer_dtype(df[col]) 
+                or pd.api.types.is_float_dtype(df[col])
+                ):
 
-            if pd.api.types.is_integer_dtype(df[col]) or pd.api.types.is_float_dtype(df[col]):
                 median = np.nanmedian(df[col])
                 df[col] = df[col].fillna(median)
                 
         return df
         
+    def data_type_convt(self, df: pd.DataFrame) -> pd.DataFrame:
 
+        df = df.copy()
+        for col in df:
+            if(pd.api.types.is_string_dtype(df[col])):
+                found = False
+                for i in range(df.shape[0]):
+                    str_data = df.loc[i,col]
+
+                    if pd.isna(str_data):
+                        continue
+
+                    s = str.split(str_data)
+                    for alpha in s:
+                        if alpha.isalpha():
+                            print("verified")
+
+                            found = True
+                            break
+                if found:
+                    break
+            else:
+                print(col,"1")
+
+        return df
     
