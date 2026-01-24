73959aa5af3a3863d5759fd5d33cc690201c71f3
Blackstate9080 <is7636665@gmail.com>
2026-01-24 23:49:56 +0530

    Stil Error in int conversion

--- AutoCleanPipeline/src/cleaner.py
+++ AutoCleanPipeline/src/cleaner.py
@@ -1,4 +1,4 @@
-import pandas as pd
+﻿import pandas as pd
 import numpy as np
 from pandas.api.types import is_string_dtype
 
@@ -32,10 +32,11 @@ class column_cleaner:
         return df
         
     def data_type_convt(self, df: pd.DataFrame) -> pd.DataFrame:
-
+        #str to str 
         df = df.copy()
         for col in df:
-            if(pd.api.types.is_string_dtype(df[col])):
+            if(pd.api.types.is_string_dtype(df[col]) 
+                or pd.api.types.is_object_dtype(df[col])):
                 found = False
                 for i in range(df.shape[0]):
                     str_data = df.loc[i,col]
@@ -46,14 +47,48 @@ class column_cleaner:
                     s = str_data.split()
                     for alpha in s:
                         if alpha.isalpha():
-                            df[col] = df[col].astype(str)
+                            df[col] = df[col].convert_dtypes()
                             found = True
                             break
 
-                if found:
-                    break
+                    if found:
+                        break
             else:
                 print(col,"1")
 
+        # int
+        
+        # int / numeric detection
+
+        for col in df:
+            if (pd.api.types.is_string_dtype(df[col]) or
+                pd.api.types.is_object_dtype(df[col])):
+
+                alpha_found = False
+
+                for i in range(df.shape[0]):
+                    str_data = df.loc[i, col]
+
+                    if pd.isna(str_data):
+                        continue
+
+                    s = str(str_data).split()
+                    for token in s:
+                        if token.isalpha():
+                            alpha_found = True
+                            break
+
+                    if alpha_found:
+                        break
+
+                # 🔑 decision AFTER full scan
+                if not alpha_found:
+                    df[col] = df[col].astype(float)
+
+            else:
+                print(col, "1")
+
+        print(df.dtypes)
         return df
+
     
