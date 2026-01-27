711ec10c3b5d991f841945506c5399a7d173b4f7
Blackstate9080 <is7636665@gmail.com>
2026-01-27 12:12:33 +0530

    Updated visualization

--- AutoCleanPipeline/src/visualizer.py
+++ AutoCleanPipeline/src/visualizer.py
@@ -2,14 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def visual(df: pd.DataFrame):
-    fig, ax = plt.subplots()
 
     x = df[df.columns[0]]
     y = df[df.columns[1]]
 
-    ax.plot(x,y)
+    fig, axes = plt.subplots(3, 1, figsize=(8, 12))
+
+    # Line plot
+    axes[0].plot(x, y)
+    axes[0].set_title("Line Plot")
+
+    # Scatter plot
+    axes[1].scatter(x, y)
+    axes[1].set_title("Scatter Plot")
+
+    # Bar plot
+    axes[2].bar(x, y)
+    axes[2].set_title("Bar Plot")
+
+    plt.tight_layout()
     plt.show()
-
-
-
-  
