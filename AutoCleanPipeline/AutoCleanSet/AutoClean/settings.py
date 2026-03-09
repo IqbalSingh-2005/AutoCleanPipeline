42fdf610525a6642e4cac66fd4f9e1c3252ebbb0
Blackstate9080 <is7636665@gmail.com>
2026-03-10 01:51:52 +0530

    add autocleanpipeline.onrender.com to ALLOWED_HOSTS

--- AutoCleanPipeline/AutoCleanSet/AutoClean/settings.py
+++ AutoCleanPipeline/AutoCleanSet/AutoClean/settings.py
@@ -29,6 +29,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') + [
     'dashboard.sparksx.me',
     'www.dashboard.sparksx.me',
+    'autocleanpipeline.onrender.com',
     '.onrender.com',
 ]
 
